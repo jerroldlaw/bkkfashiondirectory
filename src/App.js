0b80b85e2e3f17571f7672d70b13b6b9a5ee950b
Jerrold Law <jerroldlaw1@gmail.com>
2019-01-08 08:56:44 +0800

    changed images

--- src/App.js
+++ src/App.js
@@ -73,7 +73,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Bangkok Facebook Fashion Directory
           </p>
